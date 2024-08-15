147c1e126f43795bdca596da766351f3216dbefd
amirfarzamnia <amir2452fa@gmail.com>
2024-08-15 07:57:54 +0330

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NotFound from "./components/NotFound";
-import ReactDOM from "react-dom/client";
-import Home from "./components/Home";
-import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound';
+import ReactDOM from 'react-dom/client';
+import Home from './components/Home';
+import React from 'react';
 
-import "./index.css";
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <div className="bg-black text-white min-h-screen">
             <div className="star"></div>
