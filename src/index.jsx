0f0251e3b1236c5f4239b469b797a215382365a8
amirfarzamnia <amir2452fa@gmail.com>
2024-08-15 07:42:13 +0330

    init

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotFound from "./components/NotFound";
+import ReactDOM from "react-dom/client";
+import Home from "./components/Home";
+import React from "react";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <div className="bg-teal-900">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Router>
+        </div>
+    </React.StrictMode>
+);
