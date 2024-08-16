39e97e338597d8341c6439545c745b4ceabdf323
amirfarzamnia <amir2452fa@gmail.com>
2024-08-16 20:02:25 +0330

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5/react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { FaXTwitter, FaRegChartBar } from 'react-icons/fa6';
+import { FaTelegram, FaRegUser } from 'react-icons/fa';
 import { BsArrowLeftShort } from 'react-icons/bs';
 import Disclaimer from './components/Disclaimer';
 import { MdDashboard } from 'react-icons/md';
 import NotFound from './components/NotFound';
-import { FaTelegram } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 import ReactDOM from 'react-dom/client';
 import Home from './components/Home';
 import Dapp from './components/Dapp';
@@ -45,24 +45,26 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <BrowserRouter>
                 <header className="border-b border-b-white flex gap-2 items-center justify-between px-8 py-2">
                     <h1 className="font-bold text-xl">Passive Spectators</h1>
-                    {window.location.pathname === '/' ? (
-                        <div className="flex gap-2 items-center">
-                            <a href="/" className="border border-white hover:bg-white hover:text-black duration-200 text-xl px-4 flex items-center gap-1 rounded-sm">
-                                <FaRegChartBar />
-                                Statics
+                    <div className="flex gap-2 items-center">
+                        {window.location.pathname === '/' ? (
+                            <>
+                                <a href="/dapp" className="border border-white hover:bg-white hover:text-black duration-200 text-xl px-4 py-1 flex items-center gap-2 rounded-sm">
+                                    <MdDashboard />
+                                    Dapp
+                                </a>
+                                <a href="/login" className="border border-white hover:bg-white hover:text-black duration-200 text-xl px-4 py-1 flex items-center gap-2 rounded-sm">
+                                    <FaRegUser />
+                                    Dashboard
+                                </a>
+                            </>
+                        ) : (
+                            <a href="/" className="border border-white hover:bg-white hover:text-black duration-200 text-xl px-4 py-1 flex items-center gap-2 rounded-sm group">
+                                <BsArrowLeftShort className="group-hover:-translate-x-1 transform duration-200" />
+                                Back
                             </a>
-                            <a href="/dapp" className="border border-white hover:bg-white hover:text-black duration-200 text-xl px-4 flex items-center gap-1 rounded-sm">
-                                <MdDashboard />
-                                Dapp
-                            </a>
-                            <w3m-button />
-                        </div>
-                    ) : (
-                        <a href="/" className="border border-white hover:bg-white hover:text-black duration-200 text-xl px-4 flex items-center gap-1 rounded-sm group">
-                            <BsArrowLeftShort className="group-hover:-translate-x-1 transform duration-200" />
-                            Back
-                        </a>
-                    )}
+                        )}
+                        <w3m-button />
+                    </div>
                 </header>
                 <Routes>
                     <Route path="/" element={<Home />} />
