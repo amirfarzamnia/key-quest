3bc49ab1925f94d6d71e66cce532a99e98be854b
amirfarzamnia <amir2452fa@gmail.com>
2024-08-17 20:52:58 +0330

    update

--- src/index.jsx
+++ src/index.jsx
@@ -91,7 +91,6 @@ function App() {
                             Back
                         </a>
                     )}
-                    <w3m-button />
                 </div>
             </header>
             <BrowserRouter>
